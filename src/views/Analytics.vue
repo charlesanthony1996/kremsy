b486345016e96f88e83bad85881ce8c55f38c990
Charles Anthony <charlesanthony296@gmail.com>
2026-03-31 15:19:34 +0200

    finally getting accuracy

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -13,7 +13,7 @@
             <v-col cols="12" md="3">
                 <v-card>
                     <v-card-title>Model Accuracy</v-card-title>
-                    <div class="text-h5">{{ metrics ? metrics.accuracy_mean.toFixed(3) : 'Loading...' }}</div>
+                    <div class="text-h5">{{ metrics ? metrics.accuracy.toFixed(3) : 'Loading...' }}</div>
                     <div class="text-green">+12%</div>
                 </v-card>
             </v-col>
@@ -91,18 +91,76 @@ const metrics = ref<any>(null)
 //   }
 // })
 
+// onMounted(async () => {
+//   try {
+//     const res = await fetch(
+//       "https://cdn.jsdelivr.net/gh/charlesanthony1996/just-a-little-foggy@main/metrics.json"
+//     )
+
+//     if (!res.ok) throw new Error("Fetch failed")
+
+//     const data = await res.json()
+//     metrics.value = data
+
+//     console.log("Loaded metrics:", data)
+//   } catch (err) {
+//     console.error(err)
+//   }
+// })
+
+// onMounted(async () => {
+//   try {
+//     const res = await fetch(
+//       "https://tmhwaeusylkfxplqyzxf.supabase.co/rest/v1/runs?select=*&order=created_at.desc&limit=1",
+//       {
+//         headers: {
+//           apikey: "YOUR_PUBLIC_ANON_KEY",
+//           Authorization: "Bearer YOUR_PUBLIC_ANON_KEY"
+//         }
+//       }
+//     )
+
+//     if (!res.ok) throw new Error("Fetch failed")
+
+//     const data = await res.json()
+
+//     console.log("Supabase data:", data)
+
+//     metrics.value = data[0]
+
+//   } catch (err) {
+//     console.error(err)
+//   }
+// })
+
+const SUPABASE_URL = "https://tmhwaeusylkfxplqyzxf.supabase.co"
+// const SUPABASE_ANON_KEY = "YOUR_ANON_PUBLIC_KEY"
+const SUPABASE_ANON_KEY = "sb_publishable_SjEXm8SksDhy8CurunJO-w_oej1v8qj"
+
 onMounted(async () => {
   try {
     const res = await fetch(
-      "https://cdn.jsdelivr.net/gh/charlesanthony1996/just-a-little-foggy@main/metrics.json"
+      `${SUPABASE_URL}/rest/v1/runs?select=*&order=created_at.desc&limit=1`,
+      {
+        headers: {
+          apikey: SUPABASE_ANON_KEY,
+          Authorization: `Bearer ${SUPABASE_ANON_KEY}`
+        }
+      }
     )
 
-    if (!res.ok) throw new Error("Fetch failed")
+    if (!res.ok) {
+      const text = await res.text()
+      console.error("Supabase error:", text)
+      throw new Error("Fetch failed")
+    }
 
     const data = await res.json()
-    metrics.value = data
 
-    console.log("Loaded metrics:", data)
+    console.log("Supabase data:", data)
+
+    metrics.value = data[0]
+
   } catch (err) {
     console.error(err)
   }
